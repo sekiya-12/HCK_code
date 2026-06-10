40f1f49fa6bf96ddaef2652d4361c359d08185b0
TakumiKondou <takumi03k@gmail.com>
2026-06-10 15:40:46 +0900

    Delete TromboneUnit directory
    clientと重複しているため．

--- TromboneUnit/Secret.h
+++
@@ -1,8 +0,0 @@
-#ifndef SECRET_H
-#define SECRET_H
-
-const char *ssid = "YOUR_WIFI_SSID"; //SSIDを入力する
-const char *password = "YOUR_WIFI_PASSWORD"; //WiFiパスワードを入力する
-const unsigned int localUdpPort = 3000;
-
-#endif
